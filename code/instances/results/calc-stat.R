48dd34cb58e63f0bc7424bef5f8afc42750da7c8
Lars Relund <junk@relund.dk>
2024-02-15 12:40:13 +0100

    Remove test

--- code/instances/results/calc-stat.R
+++ code/instances/results/calc-stat.R
@@ -39,7 +39,7 @@ zz <- file(here::here("code/instances/results/calc-stat.log"), open = "wt")
 sink(zz, type = "output")   # open the file for output
 sink(zz, type = "message")  # open the same file for messages, errors and warnings
 
-paths <- fs::dir_ls(here::here("code/instances/results"), recurse = T, type = "file", glob = "*prob*.json")[1:4]
+paths <- fs::dir_ls(here::here("code/instances/results"), recurse = T, type = "file", glob = "*prob*.json")
 lst <- map(paths, calcStat)
 res <- unlist(lst)
 names(res) <- NULL
