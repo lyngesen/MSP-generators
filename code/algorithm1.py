217e8749088e8debae3633d0999c6e194235b688
lyngesen <mark_lyngesen@live.dk>
2024-02-15 14:58:57 +0100

    Update algorithm1.py

--- code/algorithm1.py
+++ code/algorithm1.py
@@ -62,24 +62,26 @@ def main():
     # all_problems = [problem_name for problem_name in all_problems if "50" in problem_name]
     # all_problems = [problem_name for problem_name in all_problems if " in problem_name]
     # print(f"{all_problems[:10]=}")
+    save_prefix = "alg1-"
     for problem_name in all_problems:
-        if problem_name in os.listdir("instances/results/algorithm1/"):
+        if problem_name.split(".")[-1] != "json": continue
+        if save_prefix + problem_name in os.listdir("instances/results/algorithm1/"):
             print(f"  problem solved - skipping {problem_name}")
             continue
         MSP = MinkowskiSumProblem.from_json("instances/problems/"+ problem_name) 
-        print(f"{MSP=}")
-        print(f"{get_Y_max_size(MSP)=}")
 
-        if MSP.dim != 2:
+        if MSP.dim == 2:
             continue
 
-        if get_Y_max_size(MSP) > 1_000_000:
+        if get_Y_max_size(MSP) > 5_000_000:
             print(f"  problem too big (skipping): {get_Y_max_size(MSP)=}")
             continue
 
+        print(f"{MSP=}")
+        print(f"{get_Y_max_size(MSP)=}")
         Yn = methods.MS_sequential_filter(MSP.Y_list)
         # Yn = methods.MS_sequential_filter(MSP.Y_list)
-        Yn.save_json("instances/results/algorithm1/alg1-" + problem_name)
+        Yn.save_json("instances/results/algorithm1/"  +  save_prefix + problem_name)
         print(f"{len(Yn)=}")
 
         print_timeit()
@@ -88,35 +90,47 @@ def main():
 
 def test_times():
 
-    MSP = MinkowskiSumProblem.from_json("instances/problems/prob-3-200|200-uu-2_1.json") 
-    MSP = MinkowskiSumProblem.from_json("instances/problems/prob-3-200|200-uu-2_1.json") 
+    MSP = MinkowskiSumProblem.from_json("instances/problems/prob-2-50|50-ll-2_1.json") 
+    # MSP = MinkowskiSumProblem.from_json("instances/problems/prob-3-200|200-uu-2_1.json") 
     # MSP = MinkowskiSumProblem.from_json("instances/problems/prob-3-50|50-ll-2_1.json") 
 
-      
-    print(f"{MSP=}")
-    Yn = methods.MS_sequential_filter(MSP.Y_list, filter_alg = methods.KD_filter)
-    print_timeit()
-    print(f"{len(Yn)=}")
-    reset_timeit()
-    Yn = methods.MS_sequential_filter(MSP.Y_list, filter_alg = methods.naive_filter)
-    print_timeit()
 
+    Y = methods.MS_sum(MSP.Y_list)
+    Yn = methods.MS_sequential_filter(MSP.Y_list)
+    
+    Y.plot(l="Y")
+    Yn.plot(l = "Yn")
     print(f"{len(Yn)=}")
+
+    plt.show()
+      
+#     print(f"{MSP=}")
+    # Yn = methods.MS_sequential_filter(MSP.Y_list, filter_alg = methods.KD_filter)
+    # print_timeit()
+    # print(f"{len(Yn)=}")
+    # reset_timeit()
+    # Yn = methods.MS_sequential_filter(MSP.Y_list, filter_alg = methods.naive_filter)
+    # print_timeit()
+
 
 def json_files_to_csv():
 
-    with open('instances/results/RQ1.csv', 'w') as csv_file:
-        writer = csv.writer(csv_file, delimiter = ";")
+    with open('instances/results/algorithm1/results.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter = ",")
         
-
-
         # writer.writerow(";".join(("problem", "dim", "|Yn|")))
-        writer.writerow(("problem", "dim", "|Yn|"))
-
+        statistics_keys = ["p", "supported", "extreme", "unsupported"]
+        writer.writerow(("problem", "|Yn|", "|Y|", *statistics_keys ))
 
         for json_file in os.listdir("instances/results/algorithm1/"):
+            if json_file.split(".")[-1] != "json": continue
+            json_problem_file = json_file.split("alg1-")[1]
+            print(f"{json_file=}")
             Yn = PointList.from_json( "instances/results/algorithm1/" + json_file)
-            row_data = [json_file, str(Yn.dim), str(len(Yn)) ]
+            statistics = list(Yn.statistics.values())
+            statistics_values =  [Yn.statistics[key][0] for key in statistics_keys]
+            MSP = MinkowskiSumProblem.from_json("instances/problems/" + json_problem_file)
+            row_data = [json_problem_file, str(len(Yn)), len(methods.MS_sum(MSP.Y_list).removed_duplicates()), *statistics_values]
             # row = ";".join(row_data)
             writer.writerow(row_data)
 
@@ -125,7 +139,7 @@ def json_files_to_csv():
 
 
 if __name__ == "__main__":
-    # test_times()
+    test_times()
     # example_Yn()
-    main()
-    json_files_to_csv()
+    # main()
+    # json_files_to_csv()
