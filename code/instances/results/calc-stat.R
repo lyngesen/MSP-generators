6d951cf71b3d3b8fdd7a04aa593273ec0f7c26f6
Lars Relund <junk@relund.dk>
2024-02-16 09:17:14 +0100

    Add loop and timer

--- code/instances/results/calc-stat.R
+++ code/instances/results/calc-stat.R
@@ -1,6 +1,5 @@
 ## Code for classification of nondominated points and updating statistics
 
-
 library(tidyverse)
 library(gMOIP)
 here::i_am("code/instances/results/calc-stat.R")  # specify relative path given project
@@ -9,11 +8,13 @@ here::i_am("code/instances/results/calc-stat.R")  # specify relative path given 
 #' Classify and update statistics
 #'
 #' @param path Path to result file.
-#' @return The path if modified the file; otherwise `NULL`.
+#' @return True if calculated statistics.
 calcStat <- function(path) {
-   res <- NULL
+   tictoc::tic()
+   cat("Check:", path, "...")
    lst <- jsonlite::read_json(path, simplifyVector = T)
-   if (any(is.na(lst$points$cls))) {
+   calc <- any(is.na(lst$points$cls))
+   if (calc) {
       p <- lst$statistics$p
       pts <- classifyNDSet(lst$points[, 1:p])
       pts <- pts %>% distinct()
@@ -27,9 +28,12 @@ calcStat <- function(path) {
       lst$statistics$width <- Rfast::colrange(as.matrix(pts[, 1:p]))
       lst$statistics$method <- NULL
       jsonlite::write_json(lst, path, pretty = TRUE)
-      res <- path
+      cat(" done.\n")
+   } else {
+      cat(" already calc.\n")
    }
-   return(invisible(res))
+   lst <- tictoc::toc()
+   return(calc)
 }
 
 
@@ -39,12 +43,18 @@ zz <- file(here::here("code/instances/results/calc-stat.log"), open = "wt")
 sink(zz, type = "output")   # open the file for output
 sink(zz, type = "message")  # open the same file for messages, errors and warnings
 
-paths <- fs::dir_ls(here::here("code/instances/results"), recurse = T, type = "file", glob = "*prob*.json")
-lst <- map(paths, calcStat)
-res <- unlist(lst)
-names(res) <- NULL
-cat("Files updated:\n")
-res
+paths <- fs::dir_ls(here::here("code/instances/results"), recurse = T, type = "file", glob = "*prob*.json")[1:10]
+timeLimit <- 10*60  # max run time in sec
+tictoc::tic.clear()
+start <- Sys.time()
+for (path in paths) {
+   calcStat(path)
+   cpu <- Sys.time() - start 
+   if (cpu > timeLimit) {
+      message("Time limit reached! Stop R script.")
+      break
+   }
+}
 
 ## Close log file
 sink()  # close the file for output
