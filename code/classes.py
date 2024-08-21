8c89de404a2ca3a08523ff04bcd9f27be1a18d3e
lyngesen <mark_lyngesen@live.dk>
2024-08-21 18:08:40 +0200

    Update classes.py

--- code/classes.py
+++ code/classes.py
@@ -580,7 +580,7 @@ class MSPInstances:
                         'prob-4-100|100|100-lll-3_1.json',
                         'prob-4-100|100|100-mmm-3_1.json',
                         'prob-5-100|100|100-mmm-3_1.json',
-                        'prob-3-100|100|100|100-mmmm-3_1.json',
+                        'prob-3-100|100|100|100-mmmm-4_1.json',
                         # 'prob-4-200|200|200|200|200-lllll-5_5.json'
                         ]
                 self.max_instances = len(self.filename_list)
